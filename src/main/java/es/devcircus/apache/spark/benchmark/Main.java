c4111da6593e4b2ff048e9e6a5d17c15adfb9330
Adrian Novegil <adrian.novegil@gmail.com>
2015-01-09 21:22:45 +0100

    Mavenizado el proyecto. Se han añadido las dependencias para Aapche Spark Core, SQL y Hive.

---
+++ src/main/java/es/devcircus/apache/spark/benchmark/Main.java
@@ -0,0 +1,34 @@
+/**
+ * This file is part of Apache Spark Benchmark.
+ *
+ * Apache Spark Benchmark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * Apache Spark Benchmark is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package es.devcircus.apache.spark.benchmark;
+
+/**
+ * Main class
+ *
+ * @author Adrian Novegil Toledo <adrian.novegil@gmail.com>
+ */
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
